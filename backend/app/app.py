6f2ece58c0fb023bd735c780d54619414872c2d4
Nick Pontious <145630901+NPontious@users.noreply.github.com>
2025-01-31 18:57:10 -0500

    More feet

--- backend/app/app.py
+++ backend/app/app.py
@@ -9,5 +9,12 @@ def home():
         'message': 'Hello World!'
     }
 
+@app.route('/getkey')
+def getkey():
+    return {
+        # get the contents of key.txt and return it
+        'key': open('../key.txt').read()
+    }
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000) 
